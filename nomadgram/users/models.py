a156fc62838eefaa6aca785e57a244a78d537ac6
hoosan31320 <hoos31320@gmail.com>
2022-05-28 19:57:55 +0700

    Creating the User Model

--- nomadgram/users/models.py
+++ nomadgram/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, URLField, TextField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -10,11 +10,20 @@ class User(AbstractUser):
     If adding fields that need to be filled at user signup,
     check forms.SignupForm and forms.SocialSignupForms accordingly.
     """
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('not-specified', 'Not specified')
+    )
 
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+    website = URLField(null=True)
+    bio = TextField(null=True)
+    phone = CharField(max_length=140, null=True)
+    gender = CharField(max_length=80, choices=GENDER_CHOICES, null=True)
 
     def get_absolute_url(self):
         """Get url for user's detail view.
